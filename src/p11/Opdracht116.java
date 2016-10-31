60272e02abb7e01a4073e6ac92da900eb39428db
mauritsbulte <mauritsbulte@live.nl>
2016-10-31 14:44:51 +0100

    commited

---
+++ src/p11/Opdracht116.java
@@ -0,0 +1,25 @@
+package p11;
+import java.awt.*;
+import java.applet.*;
+/**
+ * Created by yoklik on 30-10-2016.
+ */
+public class Opdracht116 extends Applet {
+
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int teller;
+        int verandering = 10;
+        int x = 50;
+        int y = 50;
+
+        for(teller = 1; teller < 6; teller++) {
+            verandering += 10;
+            x -= 5;
+            y -= 5;
+            g.drawOval(x, y, verandering, verandering);
+        }
+    }
+}
