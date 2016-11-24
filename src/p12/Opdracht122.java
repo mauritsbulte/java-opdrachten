7a09584ff6376d86f7620f362fc1de148a5eeb8b
mauritsbulte <mauritsbulte@live.nl>
2016-11-24 11:37:36 +0100

    commited

---
+++ src/p12/Opdracht122.java
@@ -0,0 +1,28 @@
+package p12;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class Opdracht122 extends Applet {
+    double[] salaris;
+
+    Button knop;
+
+
+
+    public void init() {
+        salaris = new double[5];
+
+
+
+        for (int teller = 0; teller < 26; teller++) {
+            knop = new Button("klik");
+            add(knop);
+        }
+    }
+
+    public void paint(Graphics g) {
+        repaint();
+    }
+}
